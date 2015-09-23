b9e85fadef53e4fd174e0ed52cc3a60260800085
bansjuh <0882386@hr.nl>
2015-09-23 09:32:29 +0200

    Fahrenheit to celcius conferter

---
+++ Assignment4a/Assignment4a/Assignment4a.py
@@ -0,0 +1,4 @@
+﻿Fahrenheit = int(input("enter a temprature in fahrenheit: "))
+#(°F - 32) x 5/9 = °C
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+print ("Temperature:", Fahrenheit, "Fahrenheit = ", (round(Celsius,2)), " C")
