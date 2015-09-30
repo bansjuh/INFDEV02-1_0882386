b6b4d0566ee7081bf3731474c932771deb77452d
bansjuh <0882386@hr.nl>
2015-09-30 16:41:22 +0200

    Made assignment5Opdracht1 to reverse a sting

---
+++ Assignment5Opdracht1/Assignment5Opdracht1/Assignment5Opdracht1.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------------------------------
+#
+# Created by:           Julian Mansveld
+# Time Created:         30/10/2015
+# Class:                INV1B
+# Student Number:       0882386
+#
+#------------------------------------------------------------------------------------------------------
+
+name = raw_input("Hello what is your name ?\n")
+#the [::-1] option reverse the string because the end of a sting is at a index number of -1 and is going down for evry character in the string.
+print (name[::-1])
+
